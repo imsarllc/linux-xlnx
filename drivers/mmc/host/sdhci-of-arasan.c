6adc50fd8c4d3808136639d535aa14a91ee44269
Joseph Hancock <joseph.hancock@imsar.com>
2014-05-27 16:40:40 -0600

    Add linux-xlnx specific files billed to 31-8-1 Houdini II

--- drivers/mmc/host/sdhci-of-arasan.c
+++ drivers/mmc/host/sdhci-of-arasan.c
@@ -51,9 +51,15 @@ static unsigned int sdhci_arasan_get_timeout_clock(struct sdhci_host *host)
 	return freq;
 }
 
+static unsigned int sdhci_arasan_get_ro(struct sdhci_host *host)
+{
+	return 0;
+}
+
 static struct sdhci_ops sdhci_arasan_ops = {
 	.get_max_clock = sdhci_pltfm_clk_get_max_clock,
 	.get_timeout_clock = sdhci_arasan_get_timeout_clock,
+	.get_ro = sdhci_arasan_get_ro,
 };
 
 static struct sdhci_pltfm_data sdhci_arasan_pdata = {
