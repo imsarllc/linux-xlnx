24035627cf6787442f92e32995dd911e93b331c6
Brendon Jensen <brendon.jensen@imsar.com>
2024-08-21 14:28:37 -0600

    Use git describe for LOCALVERSION

--- imsar_build.sh
+++ imsar_build.sh
@@ -4,7 +4,9 @@ set -e
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
-MAKE_OPTS="-j$(nproc --ignore 2) O=kernel_out"
+version=$(git describe --tags --always)
+
+MAKE_OPTS="-j$(nproc --ignore 2) O=kernel_out LOCALVERSION=-$version"
 
 if [[ -n "$1" ]]; then
     make $MAKE_OPTS $1
